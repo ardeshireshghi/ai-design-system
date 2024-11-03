16d4712796c090d6864d69feeca20a5a31453a8e
Component Generator <ardi.eshghi@gmail.com>
2024-11-03 14:31:16 -0500

    Adds or updates src/components/Button/Button.stories.tsx file

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,24 +1,24 @@
 // Button.stories.tsx
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './Button';
 
 export default {
   title: 'Components/Button',
   component: Button,
-} as Meta;
+} as ComponentMeta<typeof Button>;
 
-const Template: Story = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
   label: 'Primary Button',
-  onClick: () => alert('Button clicked!'),
+  onClick: () => alert('Button clicked'),
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   label: 'Disabled Button',
-  onClick: () => alert('Button clicked!'),
+  onClick: () => alert('Button clicked'),
   disabled: true,
 };
