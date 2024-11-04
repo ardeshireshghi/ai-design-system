78a6f805fb3b5c211210a7f3a82def55dfbcb83c
Component Generator <ardi.eshghi@gmail.com>
2024-11-04 08:37:53 -0500

    Adds or updates tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 
-    module.exports = {
-      purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-      darkMode: false,
-      theme: { extend: {} },
-      variants: { extend: {} },
-      plugins: [],
-    };
-  
+module.exports = {
+purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+darkMode: false,
+theme: { extend: {} },
+variants: { extend: {} },
+plugins: [],
+};
+    
